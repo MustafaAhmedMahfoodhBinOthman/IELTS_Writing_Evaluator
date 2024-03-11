4914197d96bfe9852f0241418a8c7cbbaaebaa5e
Mustafa BinOthman <91588583+MustafaAhmedMahfoodhBinOthman@users.noreply.github.com>
2024-03-11 15:13:27 +0300

    Add files via upload
    remove github icon

--- ielts_writing.py
+++ ielts_writing.py
@@ -8,7 +8,14 @@ import random
 import re
 from PIL import Image
 
-
+hide_st_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            header {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_st_style, unsafe_allow_html=True)
 
 
 
